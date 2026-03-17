4fd8f1b7378035898377b994ce5e84445f548d38
Chris Ye <chye0001@kea.stud.dk>
2026-03-17 14:41:18 +0100

    mode config more readable, and modified playwright test result to be put inside the e2e folder instead of root

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,20 +13,31 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests/e2e',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: './tests/e2e/results/playwright-report', open: "on-failure" }], // for the developer to manually investigate failures after a run
+    ['junit', { outputFile: './tests/e2e/results/junit.xml' }],                              // for CI to parse and display in the build results
+    ['list'],                                                                                // like console.log prints results to terminal
+  ],
+  outputDir: './tests/e2e/results/artifacts', // traces, screenshots, videos
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:63110',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -67,7 +78,7 @@ export default defineConfig({
     // {
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    //code  },
   ],
 
   /* Run your local dev server before starting the tests */
